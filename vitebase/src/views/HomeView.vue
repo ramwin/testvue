2bbfed874f07f7d6e26c2d8da99f496c0cb660f6
Xiang Wang <ramwin@qq.com>
2023-10-29 10:44:17 +0800

    feat: jsondata的处理

--- vitebase/src/views/HomeView.vue
+++ vitebase/src/views/HomeView.vue
@@ -1,10 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue"
 import TheWelcome from '../components/TheWelcome.vue'
+interface User {
+  id: number
+  name: string
+}
+
+interface JsonUser {
+  data: {
+    detail: User
+  }
+}
+const jsonData = ref<User>()
+
+onMounted(() => {
+  const data_string = '{"data": {"detail": {"id": 12, "name": "123"}}}'
+  const json_data = JSON.parse(data_string) as JsonUser
+  jsonData.value = json_data.data.detail
+})
 
 </script>
 
 <template>
   <main>
+    <h1>json的unknow</h1>
+    {{ jsonData  }}
     <RouterLink :to="{name: 'testref'}">测试Ref</RouterLink>
     <TheWelcome />
   </main>
