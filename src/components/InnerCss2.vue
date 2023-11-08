dea4d55bb87fcb7bf2a3ac6cd7699aefb9e3c7b8
Xiang Wang <ramwin@qq.com>
2023-11-08 20:27:45 +0800

    feat: scop的用法

---
+++ src/components/InnerCss2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>我是内部</div>
+  <div class="box-container2">
+    <div class="box2"></div>
+    <div class="box2"></div>
+    <div class="box2"></div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style scoped>
+.box2 {
+  border: red 1px solid;
+  min-width: 20px;
+  min-height: 20px;
+}
+.box-container2 {
+  display: flex;
+  border: 1px solid black;
+  height: 130px;
+  align-items: center;
+}
+</style>
--- src/views/TailWind.vue
+++ src/views/TailWind.vue
@@ -8,10 +8,24 @@
       <div class="box"></div>
     </div>
     <inner-css />
+    <h1>外部设置scope以后外部的和内部的互相不影响</h1>
+    <p>外部的css变成了.box2[data-v-7e8dd523] </p>
+    <div class="box-container2">
+      <div class="box2"></div>
+      <div class="box2"></div>
+    </div>
+    <inner-css2 />
+    <h1>如果我通过div把内部框起来</h1>
+    <p>只有最外面的box变宽了，因为实际效果是 .box-container3[data-v-7e8dd523] .box2[data-v-7e8dd523] 所有外部的元素的data都一致。 所有内部的data都不一致</p>
+    <div class="box-container3">
+      <div class="box2"></div>
+      <inner-css2 />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
   import InnerCss from "../components/InnerCss.vue"
+  import InnerCss2 from "../components/InnerCss2.vue"
   import { ref } from "vue"
   let a = ref("123")
 </script>
@@ -20,5 +34,18 @@
   border: red 1px solid;
   width: 100px;
   height: 100px;
+}
+</style>
+<style scoped>
+.box2 {
+  border: red 1px solid;
+  width: 100px;
+  height: 100px;
+}
+.box-container3 {
+  .box2 {
+    width: 60px;
+    height: 30px;
+  }
 }
 </style>
