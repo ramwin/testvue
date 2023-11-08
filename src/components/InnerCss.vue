d93c55162617837362c7f6c94a1a70465a9df99a
Xiang Wang <ramwin@qq.com>
2023-11-08 20:17:53 +0800

    fix: 内部和外部会相互影响

---
+++ src/components/InnerCss.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>我是内部</div>
+  <div class="box-container">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style>
+.box {
+  border: red 1px solid;
+  width: 30px;
+  height: 30px;
+}
+.box-container {
+  display: flex;
+  border: 1px solid black;
+  height: 130px;
+  align-items: center;
+}
+</style>
--- src/views/TailWind.vue
+++ src/views/TailWind.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
-    我是vue组件{{a}}
+    <h1>外部的.box 100高影响到了内部, 内部的flex影响到了外部</h1>
+    <div class="box-container">
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </div>
+    <inner-css />
   </div>
 </template>
 <script setup lang="ts">
+  import InnerCss from "../components/InnerCss.vue"
   import { ref } from "vue"
   let a = ref("123")
 </script>
+<style>
+.box {
+  border: red 1px solid;
+  width: 100px;
+  height: 100px;
+}
+</style>
