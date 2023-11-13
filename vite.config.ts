578413bddeb1838f98c8b736ee726425e2cd5f2d
Xiang Wang <ramwin@qq.com>
2023-11-13 22:14:19 +0800

    fix: nothing

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-import AutoImport from 'unplugin-auto-import/vite'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    AutoImport({
-      imports: [
-        "vue",
-        "vue-router",
-      ],
-      dirs: [
-        "./src/components",
-      ],
-      vueTemplate: true,
-    }),
   ],
   resolve: {
     alias: {
