bab4cd57c66379694e74f733bbb3c551ae0f07ac
Xiang Wang <ramwin@qq.com>
2023-11-13 22:12:12 +0800

    feat: 手动引入

--- src/views/TestPlugin.vue
+++ src/views/TestPlugin.vue
@@ -1,8 +1,13 @@
 <template>
     <h1>测试autoimport {{ id }}</h1>
-    <a-button type="primary" @click="id+=1"> +1</a-button>
-    <simple-component />
+    <a-button type="primary" @click="onclick"> +1</a-button>
+    <SimpleComponent />
 </template>
 <script setup lang="ts">
+import { ref } from "vue"
+import SimpleComponent from "../components/SimpleComponent.vue"
 const id = ref<number>(32)
+const onclick = () => {
+    id.value += 1
+}
 </script>
