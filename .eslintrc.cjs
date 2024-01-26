6ea8d360f6004afb67165ad263c410b804fd9cf6
Xiang Wang <ramwin@qq.com>
2024-01-26 23:20:39 +0800

    feat: 添加recommended规则

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,8 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
     '@vue/eslint-config-prettier/skip-formatting',
     'plugin:unicorn/recommended',
   ],
