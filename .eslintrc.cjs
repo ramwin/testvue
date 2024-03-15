69d62e8181c3f28bd4eacb4fefbe2060aba4ecb4
Xiang Wang <ramwin@qq.com>
2024-03-15 21:01:52 +0800

    fix: 当前max-attributes-per-line不起效果

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,9 @@ require("@rushstack/eslint-patch/modern-module-resolution")
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/base",
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-    "@vue/eslint-config-prettier/skip-formatting",
     "plugin:unicorn/recommended",
     "prettier",
   ],
--- src/views/TestTypescript.vue
+++ src/views/TestTypescript.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>ts类型强转, 默认是类型 + undefined, 最好设置初始值</h1>
+    <p class="p" style="border: solid red 1px">这里格式化的时候不会换行</p>
     <p>{{ a }}</p>
     <el-button @click="format">点我</el-button>
     <h1>测试eslint</h1>
