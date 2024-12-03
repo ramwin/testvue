76c0291958391c6a1955cabca492d0b19683ab17
Xiang Wang <ramwin@qq.com>
2024-12-03 23:10:45 +0800

    feat: 引入vxetable

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,12 @@ import router from "./router"
 
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 
+import VxeUI from 'vxe-pc-ui'
+import 'vxe-pc-ui/lib/style.css'
+import VxeUITable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
@@ -19,5 +25,7 @@ const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.use(Antd)
+
+app.use(VxeUI).use(VxeUITable)
 
 app.mount("#app")
